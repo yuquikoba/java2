90841878f2a06c165db225cca9ba4a2fb52757ff
yuqui <kobayukapc@gmail.com>
2023-06-02 13:11:15 +0900

    Thief.App追加

---
+++ ThiefApp.java
@@ -0,0 +1,16 @@
+public class ThiefApp{
+	public static void heal(int hp){
+		hp+=10;
+	}
+	public static void heal(Thief thief){
+		thief.hp+=10;
+	}
+	public static void main(String[] args){
+		int baseHp=25;
+		Thief t=new Thief("アサカ",baseHp);
+		heal(baseHp);
+		System.out.println(baseHp+":"+t.hp);
+		heal(t);
+		System.out.println(baseHp+":"+t.hp);
+	}
+}
